5fbe3e2ba4248b11033f9f1a79d7db25efaf3780
olubiyi09 <olubiyi09@gmail.com>
2022-09-15 02:14:51 +0400

    added some more function and style

---
+++ script.js
@@ -0,0 +1,23 @@
+const inputText = document.querySelector(".input-area");
+const result = document.querySelector(".output");
+const btn = document.querySelector("button");
+let filterInput;
+
+btn.addEventListener("click", checkPalindrome);
+
+function checkPalindrome(e) {
+  e.preventDefault();
+  if (inputText.value == "") {
+    alert("Nothing to check");
+  }
+  let reverseInput = filterInput.split("").reverse().join("");
+  if (filterInput != reverseInput) {
+    return (result.innerHTML = `<strong>${inputText.value}</strong> is not a Polindrome`);
+  } else {
+    return (result.innerHTML = `<strong>${inputText.value}</strong> is a Polindrome`);
+  }
+}
+
+inputText.addEventListener("keyup", () => {
+  filterInput = inputText.value.toLowerCase().replace(/[^A-Z0-9]/gi, "");
+});
